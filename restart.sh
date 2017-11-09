a02b68b8d0c5ce61cfe668d0de010e5b56acedaa
Ruslan A <ruslan.absalyamov@gmail.com>
2017-11-10 00:00:49 +0300

    restart.sh added.

--- restart.sh
+++ restart.sh
@@ -4,24 +4,8 @@ sdir="$(dirname $0)"
 
 echo "Script dir=$sdir"
 
-cd $sdir
-
-git reset --hard HEAD
-git pull
-
-mvn clean package -DskipTests
-OUT=$?
-
-if [ $OUT != 0 ]; then
-   echo "Project rebuild failed: $OUT!"
-   exit $OUT
-fi
-
 echo "Killing all started mavenzip instances..."
 pgrep -a -f mavenzip.war | awk '{print $1;}' | while read -r a; do kill -9 $a; done
-
-rm ./dist/*.war
-cp ./target/allinone-1.0.war ./dist/mavenzip.war
 
 cd $sdir/dist
 
